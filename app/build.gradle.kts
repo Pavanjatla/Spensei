64e13297992fcfafa996d0ce9f84186522fa6392
pavanjatla <pavansairamjatlaiiit@gmail.com>
2026-07-12 17:07:54 +0530

    project setup, package structure, core dependencies wired

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,16 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlin {
         jvmToolchain(17)
